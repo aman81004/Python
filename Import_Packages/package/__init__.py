2bcfe68db0bdd56fa891e260ba937b5e7d59377a
aman81004 <shaikhmohammedfazlurrahman@gmail.com>
2026-04-28 12:35:40 +0530

    Importing and Creating Packages

---
+++ Import_Packages/package/__init__.py
@@ -0,0 +1,4 @@
+'''
+__init_.py is a special file in Python that is used to indicate that the directory it is present in is a
+Python package. It can be an empty file, but it can also contain initialization code for the package. When a package is imported, the code in __init__.py is executed, allowing you to set up any necessary variables, functions, or classes that should be available when the package is imported.
+'''
